37f77e6ae4e95e4d63bacb4bddc1400fb139e817
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-10-10 18:26:28 +0600

    now i know many print method

---
+++ print_Statements.php
@@ -0,0 +1,43 @@
+<?php
+
+// method 1
+echo "Hi";
+echo "\n";
+
+// method 2
+echo ("Hi");
+echo "\n";
+
+// method 3
+echo "<h1>PHP is old</h1>";
+echo "\n";
+
+// method 4
+echo "PHP is bad!</br>";
+echo "\n";
+
+// method 5
+echo "hi","how are you","tell me";
+echo "\n";
+
+// method 6
+print "Hi";
+echo "\n";
+
+// method 7
+print ("Hi");
+echo "\n";
+
+// method 8
+print "<h1>PHP is old</h1>";
+echo "\n";
+
+// method 9
+print "PHP is bad!</br>";
+echo "\n";
+
+// method 10
+print "hi"."how are you"."tell me";
+echo "\n";
+
+?>
