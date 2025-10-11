e473689db4d8db9422414f39fc5f8aebb7f1b498
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-10-11 15:08:31 +0600

    now i know the routing and the get requist on server

--- server/my-laravel/routes/web.php
+++ server/my-laravel/routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+// api routes
+Route::get('/api', function () {
+    return [
+        "hi",
+        15,
+        "bye"
+    ];
 });
