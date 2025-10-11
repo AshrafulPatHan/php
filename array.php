de272eea8cca7229a2765e09f8cbdd66503686ff
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-10-11 10:42:11 +0600

    start learning array

---
+++ array.php
@@ -0,0 +1,19 @@
+<?php
+
+# PHP Indexed Arrays
+$array_ = array("hello","hi","tata","byby");
+var_dump($array_);
+
+print($array_[0]."\n");
+
+// all array data
+echo "All array data : \n";
+
+foreach ($array_ as $x) {
+  echo "$x \n";
+}
+echo " \n";
+
+
+
+?>
