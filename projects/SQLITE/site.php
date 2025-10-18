5f670ad1b7da1f135fd6682035fc2252d4781f69
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-10-18 22:04:16 +0600

    now i know the how php site works

---
+++ projects/SQLITE/site.php
@@ -0,0 +1,24 @@
+<?php
+
+echo '
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>test html on php</title>
+	<style>
+		h1{
+			color: blue;
+		}
+	</style>
+</head>
+<body>
+	<h1>Hello world</h1>
+</body>
+</html>
+
+';
+
+?>
