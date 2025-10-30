9b7c9c149a7eb46831c34a57586a7000e7944d73
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-10-30 20:55:44 +0600

    finish

--- algrudim/main.php
+++ algrudim/main.php
@@ -1,4 +1,5 @@
 <?php
+# find user data algrudim
 
 $json_file_path = 'user.json';
 $json_string = file_get_contents($json_file_path);
@@ -9,12 +10,12 @@ echo "\n";
 
 function FindUser($array){
     # get input
-    $input = "1";
+    $input = "Karim Ahmed";
     $result = "Invaled input";
     $foundUser = null;
     
     // chek the inpute type
-    if (is_numeric($input)) {
+    if (is_numeric($input) && substr($input, 0, 1) != "+" ) {
         $input = (int)$input;
         foreach ($array as $user) {
             if ($user['id'] === $input) {
@@ -22,18 +23,35 @@ function FindUser($array){
                 break;
             }
         }
-        $result = $foundUser['name'];
+        $result = [$foundUser['id'] ,$foundUser['name'],$foundUser['email'],$foundUser['photo'],$foundUser['phone'] ];
+    }elseif (is_numeric(substr($input, 3)) && (int)(substr($input, 3)) >10 && substr($input, 0, 1) === "+" ) {
+        foreach ($array as $user) {
+            if ($user['phone'] === $input) {
+                $foundUser = $user;
+                break;
+            }
+        }
+        $result = [$foundUser['id'] ,$foundUser['name'],$foundUser['email'],$foundUser['photo'],$foundUser['phone'] ];
+    }elseif (str_contains($input, '@')) {
+        foreach ($array as $user) {
+            if ($user['email'] === $input) {
+                $foundUser = $user;
+                break;
+            }
+        }
+        $result = [$foundUser['id'] ,$foundUser['name'],$foundUser['email'],$foundUser['photo'],$foundUser['phone'] ];
+    }elseif (is_string($input)) {
+        foreach ($array as $user) {
+            if ($user['name'] === $input) {
+                $foundUser = $user;
+                break;
+            }
+        }
+        $result = [$foundUser['id'] ,$foundUser['name'],$foundUser['email'],$foundUser['photo'],$foundUser['phone'] ];
     }
-    // elseif (condition) {
-    //     # code...
-    // }elseif (condition) {
-    //     # code...
-    // }elseif (condition) {
-    //     # code...
-    // }
     return $result;
 }
-echo FindUser($array);
+print_r (FindUser($array));
 
 echo "\n";
 
--- algrudim/test.php
+++ algrudim/test.php
@@ -1,25 +1,7 @@
 <?php
+$myString = "HelloWorld";
+$firstLetter = substr($myString, 0, 1);
+echo $firstLetter; // Output: H
 
-$users = [
-    ['id' => 1, 'name' => 'Alice', 'email' => 'alice@example.com'],
-    ['id' => 2, 'name' => 'Bob', 'email' => 'bob@example.com'],
-    ['id' => 3, 'name' => 'Charlie', 'email' => 'charlie@example.com'],
-];
-
-$userIdToFind = 2;
-$foundUser = null;
-
-foreach ($users as $user) {
-    if ($user['id'] === $userIdToFind) {
-        $foundUser = $user;
-        break; // Stop iterating once the user is found
-    }
-}
-
-if ($foundUser) {
-    echo "User found: " . $foundUser['name'] . " (" . $foundUser['email'] . ")";
-} else {
-    echo "User with ID " . $userIdToFind . " not found.";
-}
-
+echo "\n";
 ?>
